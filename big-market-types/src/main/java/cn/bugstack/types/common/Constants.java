1102c950e97698b1038c8f39383d3eefb92ac45c
LostSpirit <2495371725@qq.com>
2026-05-04 20:26:50 +0800

    14-抽奖活动订单流程设计

--- big-market-types/src/main/java/cn/bugstack/types/common/Constants.java
+++ big-market-types/src/main/java/cn/bugstack/types/common/Constants.java
@@ -29,10 +29,16 @@ public class Constants {
 
 
     public static class RedisKey {
+        public static String ACTIVITY_KEY = "big_market_activity_key_";
+
+        public static String ACTIVITY_SKU_KEY = "big_market_activity_sku_key_";
+
+        public static String ACTIVITY_COUNT_KEY = "big_market_activity_count_key_";
 
         public static String STRATEGY_KEY = "big_market_strategy_key_";
 
         public static String STRATEGY_AWARD_KEY = "big_market_strategy_award_key_";
+
         public static String STRATEGY_AWARD_LIST_KEY = "big_market_strategy_award_list_key_";
 
         public static String STRATEGY_RATE_TABLE_KEY = "big_market_strategy_rate_table_key_";
